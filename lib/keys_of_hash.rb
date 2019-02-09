59b4f82b22692918331b74ec790818777f1d67f8
Lu Daley <daleyluis@gmail.com>
2019-02-09 01:44:37 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,21 @@
+require 'pry'
+class Hash
+  def keys_of(*arguments)
+    array = []
+    arguments.each do |arg|    # code goes here
+#arguments.detect do |arg|    # code goes here
+  #binding.pry
+    end
+   end
+end
+
+# keys_of returns an array
+
+=begin
 class Hash
   def keys_of(arguments)
     # code goes here
+binding.pry
   end
 end
+=end
