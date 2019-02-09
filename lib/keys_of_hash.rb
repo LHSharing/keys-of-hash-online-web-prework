9b6e9a5bd098c960c83e380d7330af29c511f46f
Lu Daley <daleyluis@gmail.com>
2019-02-09 02:17:28 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,26 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # arguments = Panama
+    cool = []
+    self.each do |key,value|
+      arguments.each do |arg|
+        if arg == value
+        cool << key
+          # code goes here
+#arguments.detect do |arg|    # code goes here
+  #binding.pry
+           end
+        end
+     end
+     return cool
   end
 end
+
+# keys_of returns an array
+# returns red-footed tortoise in an array for the animal hash when passed the argument 'Panama' (FAILED - 1)
+# Failures:
+#   1) keys_of returns red-footed tortoise in an array for the animal hash when passed the argument 'Panama'
+#      Failure/Error: expect(result).to include("red-footed tortoise")
+#        expected ["Panama"] to include "red-footed tortoise"
+#      # ./spec/keys_of_hash_spec.rb:11:in `block (2 levels) in <top (required)>'
